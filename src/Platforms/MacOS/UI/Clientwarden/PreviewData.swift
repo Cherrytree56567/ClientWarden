cd686506a06ccfafe1ed030cab26af47d68b137b
CT5 <CT5@example.com>
2026-06-26 17:06:37 +1000

    feat: fixed bugs and added sidepane stuff

---
+++ src/Platforms/MacOS/UI/Clientwarden/PreviewData.swift
@@ -0,0 +1,18 @@
+//
+//  PreviewData.swift
+//  Clientwarden
+//
+//  Created by Ronit D on 26/6/2026.
+//
+
+import SwiftUI
+
+struct PreviewData: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PreviewData()
+}
