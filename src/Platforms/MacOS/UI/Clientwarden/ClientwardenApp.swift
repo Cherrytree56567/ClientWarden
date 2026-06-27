9daa6f8817ef68d63430a4e193a1c6cdfbe73f76
CT5 <124994670+Cherrytree56567@users.noreply.github.com>
2026-06-27 23:08:27 +1000

    Update ClientwardenApp.swift

--- src/Platforms/MacOS/UI/Clientwarden/ClientwardenApp.swift
+++ src/Platforms/MacOS/UI/Clientwarden/ClientwardenApp.swift
@@ -1,27 +1,14 @@
 import SwiftUI
 
-@Observable
-final class ToastStore {
-    var toasts: [Toast] = []
-}
-
-let g_toastStore = ToastStore()
-
-/*
- * On Release:
- * Change code to:
- * HStack(spacing: 0) {
+@main
+struct ClientwardenApp: App {
+    var body: some Scene {
+        WindowGroup("Clientwarden") {
+            HStack(spacing: 0) {
                 NavigationPanelView()
                 SidePanelView()
             }
             .frame(minWidth: 700, minHeight: 400)
- * Remove NavPanel and ItemsPanel Callback
- */
-@main
-struct ClientwardenApp: App {
-    var body: some Scene {
-        WindowGroup {
-            PreviewData().test1()
         }
     }
 }
