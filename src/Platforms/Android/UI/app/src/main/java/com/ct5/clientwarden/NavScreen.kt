825c00355120d4f9e3eace1dd32ab2133199c7fe
CT5 <124994670+Cherrytree56567@users.noreply.github.com>
2026-09-02 20:33:45 +1000

    feat: added new folder support

--- src/Platforms/Android/UI/app/src/main/java/com/ct5/clientwarden/NavScreen.kt
+++ src/Platforms/Android/UI/app/src/main/java/com/ct5/clientwarden/NavScreen.kt
@@ -1,8 +1,18 @@
 package com.ct5.clientwarden
 
 import android.content.res.Configuration
+import android.graphics.fonts.Font
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -14,23 +24,44 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import androidx.compose.ui.window.Popup
 import com.composables.icons.lucide.Archive
+import com.composables.icons.lucide.ArrowRight
 import com.composables.icons.lucide.CreditCard
+import com.composables.icons.lucide.EllipsisVertical
 import com.composables.icons.lucide.Folder
 import com.composables.icons.lucide.House
 import com.composables.icons.lucide.IdCard
@@ -41,6 +72,12 @@ import com.composables.icons.lucide.SquareAsterisk
 import com.composables.icons.lucide.Star
 import com.composables.icons.lucide.StickyNote
 import com.composables.icons.lucide.Trash2
+import com.composables.icons.lucide.X
+import com.ct5.clientwarden.ItemsScreen.cb_archive
+import com.ct5.clientwarden.ItemsScreen.cb_bin
+import com.ct5.clientwarden.ItemsScreen.cb_delete
+import com.ct5.clientwarden.ItemsScreen.cb_restore
+import com.ct5.clientwarden.ItemsScreen.cb_unarchive
 import java.util.UUID
 
 sealed interface NavItem {
@@ -82,7 +119,10 @@ object NavScreen {
     var cb_NewFolder: ((name: String) -> Unit)? = null
 
     @Composable
-    fun NavButton(text: String, icon: ImageVector, item: NavItem, onClick: () -> Unit = {}, start: Boolean = false, end: Boolean = false) {
+    fun NavButton(text: String, icon: ImageVector, item: NavItem,
+                  onClick: () -> Unit = {}, start: Boolean = false,
+                  end: Boolean = false, addEllipses: Boolean = false) {
+        var m_expanded by remember { mutableStateOf(false) }
         /*
          * A Mat You Button with a custom BG Color
          */
@@ -103,7 +143,7 @@ object NavScreen {
                 bottomStart = if (end) 12.dp else 0.dp,
                 bottomEnd = if (end) 12.dp else 0.dp
             ),
-            contentPadding = PaddingValues(horizontal = 16.dp, vertical = 0.dp),
+            contentPadding = PaddingValues(16.dp, 0.dp, 0.dp, 0.dp),
             colors = ButtonDefaults.filledTonalButtonColors(
                 containerColor = MaterialTheme.colorScheme.surfaceContainerHigh,
                 contentColor = MaterialTheme.colorScheme.surfaceContainerHigh
@@ -130,11 +170,139 @@ object NavScreen {
                     color = MaterialTheme.colorScheme.onSecondaryContainer,
                     overflow = TextOverflow.Ellipsis,
                     maxLines = 1)
+
+                Spacer(Modifier.weight(1f).fillMaxWidth())
+
+                /*
+                 * Ellipses Box
+                 * Stores the Ellipses Button and Dropdown
+                 */
+                if (addEllipses) {
+                    Box {
+                        /*
+                     * Ellipses Button
+                     */
+                        IconButton(
+                            onClick = { m_expanded = true }
+                        ) {
+                            Icon(
+                                Lucide.EllipsisVertical,
+                                contentDescription = "More",
+                                tint = MaterialTheme.colorScheme.onSecondaryContainer,
+                                modifier = Modifier.size(20.dp)
+                            )
+                        }
+
+                        val transitionState = remember { MutableTransitionState(false) }
+                        transitionState.targetState = m_expanded
+
+                        /*
+                         * Thanks to Claude for the Animated Visibility,
+                         * although the Popup stuff was mine
+                         */
+                        if (transitionState.currentState || transitionState.targetState) {
+                            Popup(
+                                alignment = Alignment.TopEnd,
+                                onDismissRequest = { m_expanded = false }
+                            ) {
+                                androidx.compose.animation.AnimatedVisibility(
+                                    visibleState = transitionState,
+                                    enter = fadeIn(tween(120)) + scaleIn(
+                                        initialScale = 0.85f,
+                                        animationSpec = tween(120),
+                                        transformOrigin = androidx.compose.ui.graphics.TransformOrigin(
+                                            1f,
+                                            0f
+                                        )
+                                    ),
+                                    exit = fadeOut(tween(100)) + scaleOut(
+                                        targetScale = 0.85f,
+                                        animationSpec = tween(280),
+                                        transformOrigin = androidx.compose.ui.graphics.TransformOrigin(
+                                            1f,
+                                            0f
+                                        )
+                                    )
+                                ) {
+                                    Surface(
+                                        shape = RoundedCornerShape(8.dp),
+                                        color = MaterialTheme.colorScheme.surfaceContainerHighest,
+                                        tonalElevation = 3.dp,
+                                        modifier = Modifier.padding(
+                                            horizontal = 8.dp,
+                                            vertical = 0.dp
+                                        )
+                                    ) {
+                                        Column(
+                                            modifier = Modifier.padding(
+                                                horizontal = 0.dp,
+                                                vertical = 0.dp
+                                            )
+                                        ) {
+                                            /*
+                                             * Rename Folder Button
+                                             */
+                                            TextButton(
+                                                onClick = {
+                                                    m_expanded = false
+                                                    /*
+                                                     * TODO: Add rename Folder support
+                                                     */
+                                                },
+                                                shape = RoundedCornerShape(
+                                                    topStart = 8.dp,
+                                                    topEnd = 8.dp,
+                                                    bottomStart = 0.dp,
+                                                    bottomEnd = 0.dp
+                                                ),
+                                                modifier = Modifier.width(100.dp)
+                                            ) {
+                                                Text(
+                                                    "Rename",
+                                                    textAlign = TextAlign.Left,
+                                                    modifier = Modifier.fillMaxWidth()
+                                                )
+                                            }
+
+                                            /*
+                                             * Delete Folder Button
+                                             */
+                                            TextButton(
+                                                onClick = {
+                                                    m_expanded = false
+                                                    /*
+                                                     * TODO: Add delete Folder support
+                                                     */
+                                                },
+                                                shape = RoundedCornerShape(
+                                                    topStart = 8.dp,
+                                                    topEnd = 8.dp,
+                                                    bottomStart = 0.dp,
+                                                    bottomEnd = 0.dp
+                                                ),
+                                                modifier = Modifier.width(100.dp)
+                                            ) {
+                                                Text(
+                                                    "Delete",
+                                                    textAlign = TextAlign.Left,
+                                                    modifier = Modifier.fillMaxWidth()
+                                                )
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
     }
+
     @Composable
     fun view() {
+        var addFolder_expanded by remember { mutableStateOf(false) }
+        var s_folderName by remember { mutableStateOf("New Folder") }
         Column(
             modifier = Modifier.fillMaxSize()
                                .verticalScroll(rememberScrollState())
@@ -172,22 +340,96 @@ object NavScreen {
 
             /*
              * Folder Nav Items
-             * TODO: Add Ellipses to Rename or Delete folder
              */
             for ((i, folder) in folders.withIndex()) {
                 NavButton(folder.name, Lucide.Folder,
                     NavItem.Folder(folder.uuid), start = i == 0,
-                    onClick = { ItemsScreen.SetItems(cb_Folder?.invoke(folder.uuid) ?: emptyList()) })
+                    onClick = {
+                        ItemsScreen.SetItems(cb_Folder?.invoke(folder.uuid) ?: emptyList())
+                    }, addEllipses = true)
             }
 
             /*
              * Add Folder Button
-             * TODO: Add full screen popup that asks for folder
-             *  name and then creates folder
              */
             NavButton("Add Folder", Lucide.Plus, NavItem.None,
                 start = if (folders.size == 0) true else false, end = true,
-                onClick = { ItemsScreen.SetItems(cb_Card?.invoke() ?: emptyList()) })
+                onClick = {
+                    addFolder_expanded = true
+                })
+        }
+
+        if (addFolder_expanded) {
+            Dialog(
+                onDismissRequest = {
+                    addFolder_expanded = false
+                },
+                properties = DialogProperties(
+                    usePlatformDefaultWidth = false
+                ),
+            ) {
+                Surface(color = MaterialTheme.colorScheme.background, shape = RoundedCornerShape(32.dp)) {
+                    Column(
+                        modifier = Modifier
+                            .padding(24.dp)
+                            .width(300.dp),
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Text(
+                            text = "Create Folder",
+                            style = MaterialTheme.typography.titleLarge,
+                            fontWeight = FontWeight.Bold
+                        )
+
+                        Spacer(Modifier.height(24.dp))
+
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            /*
+                             * Custom Text Field
+                             * We are using a custom text field here
+                             * bc the mat you one has to be thicc, but
+                             * I need it to be thin
+                             */
+                            BasicTextField(
+                                value = s_folderName,
+                                onValueChange = {
+                                    s_folderName = it
+                                },
+                                modifier = Modifier.weight(1f)
+                                    .height(34.dp)
+                                    .background(
+                                        color = MaterialTheme.colorScheme.surfaceContainerHighest,
+                                        shape = RoundedCornerShape(32.dp)
+                                    )
+                                    .padding(horizontal = 14.dp, vertical = 6.dp),
+                                singleLine = true,
+                                textStyle = MaterialTheme.typography.bodyMedium.copy(
+                                    color = MaterialTheme.colorScheme.onSurface
+                                ),
+                                cursorBrush = SolidColor(
+                                    MaterialTheme.colorScheme.primary
+                                )
+                            )
+
+                            Spacer(Modifier.width(12.dp))
+
+                            FilledIconButton(
+                                onClick = {
+                                    addFolder_expanded = false
+                                    cb_NewFolder?.invoke(s_folderName)
+                                },
+                                modifier = Modifier.size(32.dp)
+                            ) {
+                                Icon(
+                                    Lucide.ArrowRight,
+                                    contentDescription = "More",
+                                    modifier = Modifier.size(18.dp)
+                                )
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
