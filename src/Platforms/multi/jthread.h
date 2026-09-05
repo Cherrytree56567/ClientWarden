5ac4ffc9308f0f225661c2592abab79dfbd4c1d4
CT5 <124994670+Cherrytree56567@users.noreply.github.com>
2026-09-05 10:36:02 +1000

    fix: workdflow take 28

--- src/Platforms/multi/jthread.h
+++ src/Platforms/multi/jthread.h
@@ -1,14 +1,8 @@
 #pragma once
 
-#if defined(ANDROID) || !defined(__cpp_lib_jthread)
-  #include <nonstd/jthread.hpp>
-  using nonstd::jthread;
-  using nonstd::stop_token;
-  using nonstd::stop_source;
-#else
+
   #include <thread>
   #include <stop_token>
   using std::jthread;
   using std::stop_token;
   using std::stop_source;
-#endif
