cc25aee0e799f048f129813e2905e166a23e9455
CT5 <124994670+Cherrytree56567@users.noreply.github.com>
2026-09-09 17:13:03 +1000

    fix: workflow take 43

--- src/Platforms/Android/UI/build.gradle.kts
+++ src/Platforms/Android/UI/build.gradle.kts
@@ -3,3 +3,23 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
 }
+
+android {
+    defaultConfig {
+        ndk {
+            abiFilters += listOf("arm64-v8a")
+        }
+    }
+
+    sourceSets {
+        getByName("main") {
+            jniLibs.srcDirs("src/main/jniLibs")
+        }
+    }
+
+    packaging {
+        jniLibs {
+            useLegacyPackaging = false
+        }
+    }
+}
--- src/Platforms/Android/UI/settings.gradle.kts
+++ src/Platforms/Android/UI/settings.gradle.kts
@@ -22,25 +22,5 @@ dependencyResolutionManagement {
     }
 }
 
-android {
-    defaultConfig {
-        ndk {
-            abiFilters += listOf("arm64-v8a")
-        }
-    }
-
-    sourceSets {
-        getByName("main") {
-            jniLibs.srcDirs("src/main/jniLibs")
-        }
-    }
-
-    packaging {
-        jniLibs {
-            useLegacyPackaging = false
-        }
-    }
-}
-
 rootProject.name = "Clientwarden"
 include(":app")
